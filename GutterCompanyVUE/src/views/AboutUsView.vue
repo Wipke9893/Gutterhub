a2dfa2afcf68714e66e01dd03377849d2fb22c2d
Griffin <griffinmcfarland3@gmail.com>
2024-01-03 12:44:41 -0800

    about

---
+++ GutterCompanyVUE/src/views/AboutUsView.vue
@@ -0,0 +1,27 @@
+<template>
+
+  <div>
+    <p>We are a family owned and operatored gutter solution company. Specializing in custom solutions for each client
+      while striving for excellence.  We offer many solutions in gutters to enchance your curb appeal as well as solve 
+    your drainage issues.</p>
+  </div>
+
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  created() {
+
+  },
+}
+</script>
+
+<style scoped >
+
+</style>
