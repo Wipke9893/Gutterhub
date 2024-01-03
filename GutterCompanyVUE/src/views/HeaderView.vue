008d12876c369d7e038a7c7c64be90b5280b3b34
Griffin <griffinmcfarland3@gmail.com>
2024-01-03 15:20:27 -0800

    schedule

--- GutterCompanyVUE/src/views/HeaderView.vue
+++ GutterCompanyVUE/src/views/HeaderView.vue
@@ -3,6 +3,7 @@
   <header class="header">
     <div class="nav">
       <router-link v-bind:to="{ name: 'home' }" class="nav-link">Home</router-link>
+      <router-link v-bind:to="{ name: 'schedule' }" class="nav-link">Schedule</router-link>
       <router-link v-bind:to="{ name: 'login' }" v-if="!$store.state.token" class="nav-link">Login</router-link>
       <router-link v-bind:to="{ name: 'register' }" v-if="!$store.state.token" class="nav-link">Register</router-link>
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token" class="nav-link">Logout</router-link>
---
+++ GutterCompanyVUE/src/views/ScheduleView.vue
@@ -0,0 +1,50 @@
+<template>
+  <Section>
+    <div class="header">
+      <HeaderView />
+    </div>
+    <div>
+      <Scheduler/>
+    </div>
+
+    <div class="footer">
+      <FooterView />
+    </div>
+  </Section>
+</template>
+
+<script>
+import HeaderView from "@/views/HeaderView.vue";
+import FooterView from '@/views/FooterView.vue';
+import Scheduler from "@/components/Scheduler.vue";
+export default {
+
+  data() {
+    return {
+    }
+  },
+  created() {
+  },
+  components: {
+    HeaderView,
+    Scheduler,
+    FooterView,
+  }
+}
+</script>
+
+<style scoped >
+
+section {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  margin-bottom: 5rem;
+}
+
+
+
+
+
+
+</style>
