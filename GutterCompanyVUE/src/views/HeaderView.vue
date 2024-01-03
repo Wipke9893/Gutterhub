866897301653248e9af7200368149cdbdcf01159
Moonwolf56 <loganvesterfelt@gmail.com>
2024-01-03 16:41:55 -0500

    m

--- GutterCompanyVUE/src/views/HeaderView.vue
+++ GutterCompanyVUE/src/views/HeaderView.vue
@@ -25,15 +25,6 @@ export default {
 </script>
 
 <style scoped>
-<<<<<<< HEAD
-.img {
-  background-image: url("../assets/img/gutterlogo.png");
-  height: 20rem; /* You may adjust the height as needed */
-  width: 100%;
-  background-size: 100% 250%; /* Stretch the image horizontally, maintain aspect ratio */
-  background-repeat: no-repeat;
-  background-position: center center; /* Center the image */
-=======
 
 header {
   position: fixed;
@@ -47,7 +38,6 @@ header {
   background-position: center;
   height: 20rem;
   background-color: rgba(73, 73, 73, 0.71);
->>>>>>> 29ae64fc01984a00865b8ccb1a2900f3eca84ad0
 }
 
 .header {
@@ -55,16 +45,6 @@ header {
 }
 .nav {
   display: flex;
-<<<<<<< HEAD
-  justify-content: space-around;
-  align-items: center;
-  padding: 1px;
-  height: 3rem;
-}
-
-.nav-link {
-  color: #000000;
-=======
   justify-content: space-between;
   margin-top: 20rem;
   background-color: rgb(0, 160, 213);
@@ -73,22 +53,10 @@ header {
 
 .nav-link {
   color: white;
->>>>>>> 29ae64fc01984a00865b8ccb1a2900f3eca84ad0
   text-decoration: none;
   padding: 0.5rem 1rem;
   font-size: 1.6rem;
-<<<<<<< HEAD
-  border: none;
-  background-color: #3498db;
-  border-radius: 5px;
-  transition: background-color 0.3s ease;
-}
-
-.nav-link:hover {
-  background-color: #2980b9;
-=======
   font-weight: bold;
->>>>>>> 29ae64fc01984a00865b8ccb1a2900f3eca84ad0
 }
 
 </style>
